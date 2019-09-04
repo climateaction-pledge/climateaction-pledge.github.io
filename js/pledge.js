16f84353f4c13573fc6da4c58386874db88820c7
Dan Lewis-Toakley <dan.lewistoakley@gmail.com>
2019-09-03 22:06:06 -0400

    updated styles and js to improve page layout. Added Gemfile and improved README.

---
+++ js/pledge.js
@@ -0,0 +1,28 @@
+$(document).ready(function($) {
+    $(document).on('can_embed_loaded', function() {
+        var $pledgeFormArea = $('#can-form-area-test-tech-workers-support-the-climate-strike');
+        var $PledgeForm = $('#can_sidebar');
+        var $pledgeText = $('#can_sidebar h4');
+        var $actionTitle = $('.entry-title');
+        var $pledgeImage = $('.pledge-image');
+        var $pledgeDescription = $('.action_description');
+
+        function initializeInterface() {
+            $pledgeText.addClass('pledge-text');
+            $pledgeText.insertBefore($pledgeFormArea);
+            $pledgeImage.insertBefore($actionTitle).removeClass('hidden');
+
+            var isProbablyMobile = window.innerWidth < 600;
+
+            if (isProbablyMobile) {
+                $PledgeForm.insertBefore($pledgeDescription);
+            }
+
+        }
+
+
+
+        initializeInterface();
+
+    });
+});
