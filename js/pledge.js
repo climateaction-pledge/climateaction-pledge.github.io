46e9156e8b63ab43ddbd1a70a04c805058848f07
Andrew McWilliams <andy@jahya.net>
2019-09-06 12:35:40 -0400

    Changes text of pledge counter

--- js/pledge.js
+++ js/pledge.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         var $pledgeText = $('#can-form-area-test-tech-workers-support-the-climate-strike #can_sidebar h4');
         var $pledgeDescription = $('#can-form-area-test-tech-workers-support-the-climate-strike .action_description');
         var $logOutLink = $('#can-form-area-test-tech-workers-support-the-climate-strike #log-out');
+        var $actionsText = $('#can-form-area-test-tech-workers-support-the-climate-strike .action_status_running_total');
 
         function registerHandlers() {
             $logOutLink.on('click', logoutLinkClick);
@@ -25,8 +26,13 @@ $(document).ready(function() {
             $pledgeText.hide();
         }
 
+        function replaceActionsText() {
+            $actionsText.text($actionsText.text().replace("Actions Taken", "Pledges Made"))
+        }
+
         registerHandlers();
         initializeInterface();
+        replaceActionsText();
 
     });
 });
